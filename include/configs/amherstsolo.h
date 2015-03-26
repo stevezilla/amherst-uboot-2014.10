7fe54518b4cd555601c25ccaf60305ad7ab4ff2e
Steven Scheidecker <displaypress@ubuntu.(none)>
2015-03-26 16:23:55 -0500

    Update

--- include/configs/amherstsolo.h
+++ include/configs/amherstsolo.h
@@ -17,11 +17,12 @@
 #define CONFIG_CONSOLE_DEV		"ttymxc0"
 #define CONFIG_MMCROOT			"/dev/mmcblk0p2"
 #if defined(CONFIG_MX6Q)
-#define CONFIG_DEFAULT_FDT_FILE	"imx6q-sabresd-ldo.dtb"
+#define CONFIG_DEFAULT_FDT_FILE	"imx6q-amherst-ldo.dtb"
 #elif defined(CONFIG_MX6DL)
 #define CONFIG_DEFAULT_FDT_FILE	"imx6dl-amherst-ldo.dtb"
 #endif
-#define PHYS_SDRAM_SIZE		512 * 1024 * 1024
+#define PHYS_SDRAM_SIZE		(1u * 512 * 1024 * 1024)
+#define CONFIG_NR_DRAM_BANKS		1
 
 #define CONFIG_SUPPORT_EMMC_BOOT /* eMMC specific */
 
