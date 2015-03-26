d939e6e115beb703341ce610ccce6c8006adec56
Steven Scheidecker <displaypress@ubuntu.(none)>
2015-03-26 12:58:11 -0500

    Enable PWR to PHY

--- board/handera/amherstsolo/amherstsolo.c
+++ board/handera/amherstsolo/amherstsolo.c
@@ -123,6 +123,11 @@ static int setup_fec(void)
 {
         struct iomuxc *iomuxc_regs = (struct iomuxc *)IOMUXC_BASE_ADDR;
         int ret;
+	u32 reg = 0;
+
+	reg = readl(IOMUXC_BASE_ADDR + 0x4);
+        reg |= (0x1 << 21);
+        writel(reg, IOMUXC_BASE_ADDR + 0x4);
 
         /* clear gpr1[14], gpr1[18:17] to select anatop clock */
         clrsetbits_le32(&iomuxc_regs->gpr[1], IOMUX_GPR1_FEC_MASK, 0);
