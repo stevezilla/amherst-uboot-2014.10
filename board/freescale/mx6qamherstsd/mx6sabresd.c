e1fb6d3806ad73227ebb3392b20f774fee5d8ae7
root <root@ubuntu.(none)>
2015-10-24 17:37:48 -0500

    Update

--- board/freescale/mx6qamherstsd/mx6sabresd.c
+++ board/freescale/mx6qamherstsd/mx6sabresd.c
@@ -184,7 +184,7 @@ struct fsl_esdhc_cfg usdhc_cfg[3] = {
 	{USDHC4_BASE_ADDR},
 };
 
-#define USDHC2_CD_GPIO	IMX_GPIO_NR(2, 2)
+#define USDHC2_CD_GPIO	IMX_GPIO_NR(4, 6)
 #define USDHC3_CD_GPIO	IMX_GPIO_NR(2, 0)
 
 int board_mmc_getcd(struct mmc *mmc)
