d5a123000690aa52815486ff0e9c65e8c9923b18
root <root@ubuntu.(none)>
2015-10-24 15:23:33 -0500

    Updates

--- include/configs/amherstquad.h
+++ include/configs/amherstquad.h
@@ -17,7 +17,7 @@
 #define CONFIG_CONSOLE_DEV		"ttymxc0"
 #define CONFIG_MMCROOT			"/dev/mmcblk0p2"
 #if defined(CONFIG_MX6Q)
-#define CONFIG_DEFAULT_FDT_FILE	"imx6q-amherst.dtb"
+#define CONFIG_DEFAULT_FDT_FILE	"imx6q-amherst-ldo.dtb"
 #elif defined(CONFIG_MX6DL)
 #define CONFIG_DEFAULT_FDT_FILE	"imx6dl-amherst-ldo.dtb"
 #endif
