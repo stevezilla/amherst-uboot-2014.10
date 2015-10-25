0c61ba5c7eff42612709939e16ec15c68adbe35d
root <root@ubuntu.(none)>
2015-10-25 14:38:06 -0500

    Update

--- include/configs/mx6qamherstsd.h
+++ include/configs/mx6qamherstsd.h
@@ -17,7 +17,7 @@
 #define CONFIG_CONSOLE_DEV		"ttymxc0"
 #define CONFIG_MMCROOT			"/dev/mmcblk0p2"
 #if defined(CONFIG_MX6Q)
-#define CONFIG_DEFAULT_FDT_FILE	"imx6q-amherst-ldo.dtb"
+#define CONFIG_DEFAULT_FDT_FILE	"imx6q-sabresd-ldo.dtb"
 #elif defined(CONFIG_MX6DL)
 #define CONFIG_DEFAULT_FDT_FILE	"imx6dl-sabresd-ldo.dtb"
 #endif
